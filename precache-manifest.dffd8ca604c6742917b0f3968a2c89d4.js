d5f6fad777f3b6298c1e5ff2b91ed5511f07b321
AnastasiaPlague <51404589+AnastasiaPlague@users.noreply.github.com>
2019-12-04 02:52:35 +0300

    Add files via upload

---
+++ precache-manifest.dffd8ca604c6742917b0f3968a2c89d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8657b46287d12af59899c83d12b60122",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac7eb853d5538548dc5e",
+    "url": "/static/css/main.554ee904.chunk.css"
+  },
+  {
+    "revision": "6be70d04806f04d9f4f5",
+    "url": "/static/js/2.1f6be798.chunk.js"
+  },
+  {
+    "revision": "ac7eb853d5538548dc5e",
+    "url": "/static/js/main.3b06bfbd.chunk.js"
+  },
+  {
+    "revision": "ae56d23775b2c4f55208",
+    "url": "/static/js/runtime-main.9ac91e3a.js"
+  }
+]);
